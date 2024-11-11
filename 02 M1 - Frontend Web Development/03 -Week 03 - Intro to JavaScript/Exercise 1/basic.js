192fca00cb960852e41ba2936c03b5e2a6f2e3dd
Sbahle03 <mzobesbahle03@gmail.com>
2024-11-11 11:52:38 +0200

    Add more folder

---
+++ 02 M1 - Frontend Web Development/03 -Week 03 - Intro to JavaScript/Exercise 1/basic.js
@@ -0,0 +1,27 @@
+// TODO: Create a variable called firstName and store your first name in it
+let firstName = "Sbahle"
+console.log(firstName);
+
+// TODO: Create a variable called lastName and store your last name in it
+let lastName = "Mzobe"
+console.log(lastName);
+
+// TODO: Create a variable called age using let (you'll modify this later)
+let age = 21
+console.log(age);
+
+// TODO: Create a fullName variable that combines firstName and lastName
+
+let fullName = firstName  +    lastName
+console.log(fullName);
+
+let greeting = "Hello  , "  +  fullName + "  , how are doig this morning  ?  "
+console.log(greeting);
+
+// TODO: Try changing the age variable to a different value
+
+age =22
+console.log(age);
+
+
+
---
+++ 02 M1 - Frontend Web Development/03 -Week 03 - Intro to JavaScript/Exercise 1/operation-types.js
@@ -0,0 +1,25 @@
+// Create a variable called number1 and store any number in
+
+let num1 = 18
+console.log(num1)
+// TODO: Create a variable called number2 and store a different number in it
+
+let num2 = 14
+console.log(num2)
+// TODO: Convert number1 to a string and store it in a new variable
+
+let new_num1 = String(num1)
+console.log (typeof(new_num1))
+
+// TODO: Add number1 and number2 and store the result in a variable called sum
+
+let sum = num1 + num2
+console.log(sum);
+
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+
+let product = num1 * num2
+
+
+// TODO: Print all results to the console
+console.log(product)
